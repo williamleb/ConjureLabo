7e5da376eb0403eeb995bdce5e2ab64f3e57fb62
William Lebel <william.leb@hotmail.com>
2019-09-28 17:53:25 -0400

    [Key] Creating the visual for the key

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class Hover : MonoBehaviour
+    {
+        [SerializeField] private float rotatingSpeed = 2f;
+        [SerializeField] private float verticalSpeed = 0.1f;
+        
+        [SerializeField] private float maxVerticalDistance = 0.05f;
+        
+        private bool isGoingDown;
+        
+        private Vector3 center;
+
+        private void Awake()
+        {
+            isGoingDown = false;
+
+            center = transform.position;
+        }
+
+        private void Update()
+        {
+            transform.Rotate(new Vector3(0, rotatingSpeed * Time.deltaTime, 0));
+
+            if (isGoingDown)
+            {
+                transform.position -= new Vector3(0f, verticalSpeed, 0f) * Time.deltaTime;
+            }
+            else
+            {
+                transform.position += new Vector3(0f, verticalSpeed, 0f) * Time.deltaTime;
+            }
+
+            if (transform.position.y <= center.y - maxVerticalDistance)
+            {
+                isGoingDown = false;
+            }
+            else if (transform.position.y >= center.y + maxVerticalDistance)
+            {
+                isGoingDown = true;
+            }
+        }
+    }
+}
