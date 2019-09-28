e59518a25ed1bbde04a5142ca2af8c9d745b9b6f
William Lebel <william.leb@hotmail.com>
2019-09-28 14:26:34 -0400

    [GameMenu] Allowing the user to change the game's volume in the main menu.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Game
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField] private Slider volumeSlider;
+
+        private void Start()
+        {
+            volumeSlider.value = AudioListener.volume;
+        }
+
         public void Play()
         {
             SceneManager.LoadScene(Scenes.MainScene);
@@ -22,6 +31,11 @@ namespace Game
             #if UNITY_EDITOR
                 Debug.Log("Quitting the application...");
             #endif
+        }
+
+        public void ChangeVolume()
+        {
+            AudioListener.volume = volumeSlider.value;
         }
     }
 }
