237c3c202b56ee6521ddb60768d18f3621d1ba83
William Lebel <william.leb@hotmail.com>
2019-10-08 17:28:28 -0400

    Adding a visual effect when the Sentinel is near.

---
+++ Assets/Scripts/ClaustrophobiaEffect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Game
+{
+    public class ClaustrophobiaEffect : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+        [SerializeField] private Transform sentinel;
+
+        [SerializeField] private float maxDistance = 10f;
+
+        private CanvasGroup claustrophobiaCanvasGroup;
+
+        private void Awake()
+        {
+            claustrophobiaCanvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        private void Update()
+        {
+            var distanceBetweenEntities = (player.position - sentinel.position).magnitude;
+            var alpha = 1 - (distanceBetweenEntities / maxDistance);
+
+            claustrophobiaCanvasGroup.alpha = Math.Max(0f, alpha);
+        }
+    }
+}
